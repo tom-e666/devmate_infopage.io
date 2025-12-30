4a34023eae451d488b74fd41631b50b91e56c8b4
ThaiPhucTien <tii3hc@bosch.com>
2025-12-30 15:40:41 +0700

    fix static build

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import DevmateReview from './devmate';
 
 export default function Home() {
   return (
-    // We removed 'max-w-3xl', 'py-32', 'px-16', 'items-center', and 'flex'
-    // This allows DevmateReview to take over the full screen.
     <main className="w-full min-h-screen bg-white dark:bg-black">
       <DevmateReview />
     </main>
